cbe2d459e48f0fc8ccfd290bab80384f46e0d48e
un <66024439+mapzine123@users.noreply.github.com>
2023-08-09 12:41:05 +0900

    #8 - mybatis 설정
    최소한으로 필요한 mybatis 설정 작성

--- src/main/java/org/dry/DryWebsiteApplication.java
+++ src/main/java/org/dry/DryWebsiteApplication.java
@@ -1,11 +1,12 @@
 package org.dry;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan("org.dry.mapper")
 public class DryWebsiteApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(DryWebsiteApplication.class, args);
     }
