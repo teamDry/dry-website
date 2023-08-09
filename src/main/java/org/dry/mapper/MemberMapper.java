8d24dbdae4139495c03b14e53adf4b431d135b1d
un <66024439+mapzine123@users.noreply.github.com>
2023-08-10 04:25:03 +0900

    #0 - MemberMapper 기능 추가 및 테스트
    - 주석 추가
    - data.sql 파일이 필요없게끔 test 메서드 수정
    - test 메서드의 이름을 직관적이게 변경
    - 컬럼 수를 세는 count 테스트 추가
    - 모든 컬럼을 지우는 deleteAll 테스트 추가
    - 필요없는 import 삭제

--- src/main/java/org/dry/mapper/MemberMapper.java
+++ src/main/java/org/dry/mapper/MemberMapper.java
@@ -2,9 +2,12 @@ package org.dry.mapper;
 
 import org.apache.ibatis.annotations.Mapper;
 import org.dry.entity.Member;
+import org.dry.vo.MemberIdAndPassword;
 
 @Mapper
 public interface MemberMapper {
     Member selectMemberByIdAndPassword(MemberIdAndPassword memberIdAndPassword);
-    int insertMember(Member member);
+    Integer insertMember(Member member);
+    Integer count();
+    Integer deleteAll();
 }
--- src/test/java/org/dry/mapper/MemberMapperTest.java
+++ src/test/java/org/dry/mapper/MemberMapperTest.java
@@ -1,6 +1,8 @@
 package org.dry.mapper;
 
 import org.dry.entity.Member;
+import org.dry.util.TestUtil;
+import org.dry.vo.MemberIdAndPassword;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.jdbc.EmbeddedDatabaseConnection;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
@@ -8,39 +10,71 @@ import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabas
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.mybatis.spring.boot.test.autoconfigure.MybatisTest;
-import org.springframework.transaction.annotation.Transactional;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 @DisplayName("Member Mapper Test")
-@Transactional
 @AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE, connection = EmbeddedDatabaseConnection.H2)
-@MybatisTest // default Test DB >> Test >> 없어 >> 오류
+@MybatisTest
 class MemberMapperTest {
-    @Autowired
     private MemberMapper memberMapper;
+
+    @Autowired
+    public MemberMapperTest(MemberMapper memberMapper) {
+        this.memberMapper = memberMapper;
+    }
 
     @DisplayName("Mapper Insert Test")
     @Test
     void givenNewMember_whenInsert_thenWorksFine() {
-//         given
-        Member newMember = Member.of("abc", "1234", "testMember", "test@naver.com");
+        // given
+        // 넣을 Member 객체 생성
+        Member newMember = Member.of("abcd", "1234", "testMember1", "test1@naver.com");
         // when
+        // memberMapper class와 매핑된 memberMapper.xml의 쿼리문 실행, return : insert된 컬럼 수
         int count = memberMapper.insertMember(newMember);
-//         then
-        assertThat(count == 1);
+        // then
+        // 들어간 컬럼 수가 1개면 테스트 통과
+        assertThat(count).isEqualTo(1);
     }
 
     @DisplayName("Mapper Select Test")
     @Test
-    void givenIdAndPassword_whenSelect_thenFindMember() {
+    void givenNewMember_whenSelectByIdAndPassword_thenFindMember() {
         // given
         String memberId = "abc";
         String memberPassword = "1234";
+        memberMapper.insertMember(Member.of(memberId, memberPassword, "testNick", "test@email.com"));
+
+        // memberId와 memberPassword를 합친 V.O 객체 생성
         MemberIdAndPassword memberIdAndPassword = new MemberIdAndPassword(memberId, memberPassword);
         // when
+        // selectmemberByIdAndPassword와 매핑된 쿼리 실행, return : Member
         Member findMember = memberMapper.selectMemberByIdAndPassword(memberIdAndPassword);
         // then
-        assertThat(findMember).isNull();
+        // 찾은 객체가 Null이 아니면 테스트 통과
+        assertThat(findMember).isNotNull();
+    }
+
+    @DisplayName("Mapper Count Test")
+    @Test
+    void givenTestData_whenCounting_thenWorksFine() {
+        // given
+        memberMapper.insertMember(TestUtil.getDummyData());
+        // when
+        int count = memberMapper.count();
+        // then
+        assertThat(count).isEqualTo(11);
+    }
+
+    @DisplayName("Mapper DeleteAll Test")
+    @Test
+    void givenTestData_whenDeleteAll_thenWorksFine() {
+        // given
+
+        // when
+        int deleteCount = memberMapper.deleteAll();
+        // then
+        assertThat(deleteCount).isEqualTo(10);
     }
 }
