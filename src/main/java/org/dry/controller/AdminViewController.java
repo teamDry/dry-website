204e04de3ca6b84de822b626904df85deeb7e5cf
un <66024439+mapzine123@users.noreply.github.com>
2023-08-13 05:07:23 +0900

    #21 - ViewController에 로그인 성공 / 실패 view 매핑
    임시 테스트 페이지로서 추후에 바뀔 예정

--- src/main/java/org/dry/controller/AdminViewController.java
+++ src/main/java/org/dry/controller/AdminViewController.java
@@ -8,13 +8,22 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("/admin")
 @Controller
 public class AdminViewController {
-
-    @GetMapping("/login")
-    public String getLogin() {
+    @GetMapping("/login") // 로그인 view 화면
+    public String login() {
         return "admin/login";
     }
 
-    @GetMapping("/sign-up")
+    @GetMapping("/loginSuccess") // 로그인에 성공했을 때의 url
+    public String loginSuccess() {
+        return "admin/index";
+    }
+
+    @GetMapping("/loginFail") // 로그인에 실패했을 때의 url
+    public String loginFail() {
+        return "error/admin-login-failed";
+    }
+
+    @GetMapping("/sign-up") // 회원 가입 view 화면
     public String getSignUp() {
         return "admin/sign-up";
     }
