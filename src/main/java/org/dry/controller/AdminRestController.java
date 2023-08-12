1dce1ae46c1d1f8ab5490cf0151dd0cf78d81259
un <66024439+mapzine123@users.noreply.github.com>
2023-08-13 05:10:40 +0900

    #21 - RestController Login기능 작성
    1. view에서 입력한 데이터를 Json 형태로 넘겨줌 2. 넘겨준 json객체를 IdAndPassword객체로 받음
    3. 받은 객체를 이용해 service를 통해 db에서 원하는 admin 객체를 받아옴
    4. 잘 받아왔다면 ok(200), 받아오지 못했다면 Unauthorized(401) 반환

--- src/main/java/org/dry/controller/AdminRestController.java
+++ src/main/java/org/dry/controller/AdminRestController.java
@@ -1,18 +1,15 @@
 package org.dry.controller;
 
+import jakarta.servlet.http.HttpSession;
 import org.dry.entity.Admin;
 import org.dry.service.AdminService;
 import org.dry.vo.IdAndPassword;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
-@RequestMapping("/admin")
+@RequestMapping("/api/admin")
 @RestController
 public class AdminRestController {
     private final AdminService adminService;
@@ -22,13 +19,15 @@ public class AdminRestController {
         this.adminService = adminService;
     }
 
-    @PostMapping("/login")
-    public ResponseEntity<Admin> getLogin(@RequestBody IdAndPassword idAndPassword) {
-        Admin loginAdmin = adminService.login(idAndPassword);
-        if(loginAdmin != null) {
-            return ResponseEntity.ok(loginAdmin);
+    @PostMapping("/login") // Ajax에서 작성한 IdAandPassword (Json으로 넘어올 예정), session 등록을 위한 객체
+    public ResponseEntity<Admin> login(@RequestBody IdAndPassword idAndPassword, HttpSession session) {
+        Admin loginAdmin = adminService.login(idAndPassword); // 넘어온 걸로 객체 db에서 찾아서
+        if(loginAdmin != null) { // 객체가 잘 찾아졌으면
+            session.setAttribute("loginAdmin", loginAdmin); // session에 로그인 정보 객체 담고
+            return ResponseEntity.ok(loginAdmin); // ok 응답에 로그인 정보 객체 같이 실어서 보냄
         } else {
-            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build(); // 실패했으면 실패 상태코드를 보냄
+            // .build() : json객체로 만들기
         }
     }
 
