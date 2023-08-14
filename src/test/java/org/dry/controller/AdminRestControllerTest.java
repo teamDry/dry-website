ba31ec50864dc329e790f1328228c9aaeb25942a
un <66024439+mapzine123@users.noreply.github.com>
2023-08-14 23:15:41 +0900

    #26 - AdminRestController 테스트 추가 및 수정
    추가
    - 회원 가입 완료 시 회원 가입한 아이디 넘어오는지
    
    - 중복된 아이디 검사
    - 중복되지 않은 아이디 검사
    
    - 중복된 닉네임 검사
    - 중복되지 않은 닉네임 검사
    
    - 중복된 이메일 검사
    - 중복되지 않은 이메일 검사
    
    수정
    - json 하드코딩 하여 테스트 >> 객체를 json화해서 테스트 ( `ObjectMapper` 클래스 활용)
    
    - 모든 테스트 완료

--- src/test/java/org/dry/controller/AdminRestControllerTest.java
+++ src/test/java/org/dry/controller/AdminRestControllerTest.java
@@ -1,8 +1,11 @@
 package org.dry.controller;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import jakarta.persistence.Id;
 import org.dry.entity.Admin;
 import org.dry.service.AdminService;
+import org.dry.util.TestUtil;
 import org.dry.vo.IdAndPassword;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -12,6 +15,7 @@ import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.when;
@@ -24,27 +28,125 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 class AdminRestControllerTest {
     private final MockMvc mvc;
     private final AdminRestController adminRestController;
+    // 객체를 Json화 해주는 객체
+    private final ObjectMapper objectMapper;
+
     @MockBean
     private AdminService adminService;
 
-    public AdminRestControllerTest(@Autowired MockMvc mvc, @Autowired AdminRestController adminRestController) {
+    public AdminRestControllerTest(
+            @Autowired MockMvc mvc,
+            @Autowired AdminRestController adminRestController,
+            @Autowired ObjectMapper objectMapper) {
         this.mvc = mvc;
         this.adminRestController = adminRestController;
+        this.objectMapper = objectMapper;
     }
 
 
-    @DisplayName("[rest] [post] Login 요청시 객체 반환?")
+    @DisplayName("[rest] [post] given - IdAndPassword / request - login / response - AdminObject")
     @Test
-    void givenAdminObject_whenPostRequest_thenResponseAdminObject() throws Exception {
+    void givenAdminObject_whenRequestLogin_thenResponseAdminObject() throws Exception {
         // given
         // adminService.login이 실행된다면 결과값으로 저걸 줘라
         when(adminService.login(any(IdAndPassword.class))).thenReturn(Admin.of("abc", "1234", "abcc", "a@b.c"));
+        // RestController에 넘어갈 json data
+        String jsonData = objectMapper.writeValueAsString(new IdAndPassword("abc", "1234"));
         // when & then
-        mvc.perform(post("/admin/login")
+        mvc.perform(post("/api/admin/login")
                 .contentType(MediaType.APPLICATION_JSON) // JSON 형식으로 보낼거고
-                .content("{ \"id\": \"abc\", \"password\": \"1234\" }")) // 보내는 JSON 객체는 이거임
+                .content(jsonData)) // 보내는 JSON 객체는 이거임
                 .andExpect(status().isOk()) // 응답코드가 OK(200)이냐?
                 .andExpect(jsonPath("$.id").value("abc")) // 받은 json의 id 값이 abc인가?
                 .andExpect(jsonPath("$.password").value("1234")); // 받은 json의 password 값이 "1234" 인가?
+    }
+
+    @DisplayName("[rest] [post] given -AdminObject / request - Sign-up / response - id?")
+    @Test
+    void givenAdminObject_whenRequestSignUp_thenResponseAdminId() throws Exception {
+        // given
+        when(adminService.signUp(any(Admin.class))).thenReturn(TestUtil.getDummyAdmin());
+        String jsonData = objectMapper.writeValueAsString(TestUtil.getDummyAdmin());
+        // when & then
+        mvc.perform(post("/api/admin/sign-up")
+                .contentType(MediaType.APPLICATION_JSON) // JSON 형식으로 보낼거고
+                .content(jsonData))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.id").value("testAdmin"));
+    }
+
+    // id 중복 검사
+    @DisplayName("[rest] [Get] given - NonTakenId / request - check-id / response - ok?")
+    @Test
+    void givenNonTakenAdminId_whenRequestIdCheck_thenResponseIsOk() throws Exception {
+        // given
+        // Service에 id 물어봤더니 그런 id를 가진 객체가 없다고 하면?
+        when(adminService.isIdTaken(any(String.class))).thenReturn(false);
+        String queryString = "id=testId1234";
+        // when & then
+        mvc.perform(MockMvcRequestBuilders.get("/api/admin/sign-up/check-id?" + queryString))
+                .andExpect(status().isOk());
+    }
+
+    @DisplayName("[rest] [Get] given - TakenId / request - check-id / response - badRequest?")
+    @Test
+    void givenTakenAdminId_whenRequestIdCheck_thenResponseIsBadRequest() throws Exception {
+        // given
+        // Service에 물어봤더니 그런 id를 가진 객체가 있다고 하면?
+        when(adminService.isIdTaken(any(String.class))).thenReturn(true);
+        String queryString = "id=testId1234";
+        // when & then
+        mvc.perform(MockMvcRequestBuilders.get("/api/admin/sign-up/check-id?" + queryString))
+                .andExpect(status().isBadRequest());
+    }
+
+    // 닉네임 중복 검사
+    @DisplayName("[rest] [Get] given - NonTakenNickname / request - check-nickname / response - ok?")
+    @Test
+    void givenNonTakenAdminNickname_whenRequestNicknameCheck_thenResponseIsOk() throws Exception {
+        // given
+        // Service에 물어봤더니 그런 nickname을 가진 객체가 없다고 하면?
+        when(adminService.isNicknameTaken(any(String.class))).thenReturn(false);
+        String queryString = "nickname=testNickname1234";
+        // when & then
+        mvc.perform(MockMvcRequestBuilders.get("/api/admin/sign-up/check-nickname?" + queryString))
+                .andExpect(status().isOk());
+    }
+
+    @DisplayName("[rest] [Get] given - TakenNickname / request - check-nickname / response - badRequest?")
+    @Test
+    void givenTakenAdminNickname_whenRequestNicknameCheck_thenResponseIsBadRequest() throws Exception {
+        // given
+        // Service에 물어봤더니 그런 nickname을 가진 객체가 있다고 하면?
+        when(adminService.isNicknameTaken(any(String.class))).thenReturn(true);
+        String queryString = "nickname=testNickname1234";
+        // when & then
+        mvc.perform(MockMvcRequestBuilders.get("/api/admin/sign-up/check-nickname?" + queryString))
+                .andExpect(status().isBadRequest());
+    }
+
+    // 이메일 중복 검사
+    @DisplayName("[rest] [Get] given - NonTakenEmail / request - check-email / response - ok?")
+    @Test
+    void givenNonTakenAdminEmail_whenRequestEmailCheck_thenResponseIsOk() throws Exception {
+        // given
+        // Service에 물어봤더니 그런 email을 가진 객체가 없다고 하면?
+        when(adminService.isEmailTaken(any(String.class))).thenReturn(false);
+        String queryString = "email=testEmail1234@naver.com";
+        // when & then
+        mvc.perform(MockMvcRequestBuilders.get("/api/admin/sign-up/check-email?" + queryString))
+                .andExpect(status().isOk());
+    }
+
+    @DisplayName("[rest] [Get] given - TakenEmail / request - check-email / response - badRequest?")
+    @Test
+    void givenTakenAdminEmail_whenRequestEmailCheck_thenResponseIsBadRequest() throws Exception {
+        // given
+        // Service에 물어봤더니 그런 email을 가진 객체가 있다고 하면?
+        when(adminService.isEmailTaken(any(String.class))).thenReturn(true);
+        String queryString = "email=testEmail1234@naver.com";
+        // when & then
+        mvc.perform(MockMvcRequestBuilders.get("/api/admin/sign-up/check-email?" + queryString))
+                .andExpect(status().isBadRequest());
     }
 }
