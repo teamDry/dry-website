6f30a093ac6622ca76793ba35efbf7a6eb16d53c
seonjuHa <gktjswn11@gmail.com>
2023-08-14 20:19:52 +0900

    #24 - MemberController 작성
    화면만 보여주는 ViewController
    기능을 수행하는 RestController 로 구분하여 작성

---
+++ src/main/java/org/dry/controller/MemberRestController.java
@@ -0,0 +1,42 @@
+package org.dry.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.dry.entity.Member;
+import org.dry.service.MemberService;
+import org.dry.vo.IdAndPassword;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/member")
+@Slf4j
+public class MemberRestController {
+
+    private final MemberService service;
+
+    @Autowired
+    public MemberRestController(MemberService service) {
+        this.service = service;
+    }
+
+    @PostMapping("/sign-up")
+    public Member signUp(@RequestBody Member member) {   // @Requestbody 쓰려면 jsondata로 받아와야 함, @ModelAttribute는 폼 데이터가 받아지지만, entity 설정을 setter로 해야 담아짐
+        log.debug("Member value : " + member);
+        Member newMember = service.insertMember(member);
+        log.debug("return newMember" + newMember);
+        return newMember;
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<Object> login(@RequestBody IdAndPassword idAndPassword) {
+        log.debug("return idAndPassword" + idAndPassword);
+        Member member = service.login(idAndPassword);
+        if(member != null) {
+            return ResponseEntity.ok(member);
+        }else {
+            return ResponseEntity.ok("{}");
+        }
+    }
+
+}
