99e3a187e909389184d0cfd7ba6f8c6ff233b480
un <66024439+mapzine123@users.noreply.github.com>
2023-08-14 22:27:20 +0900

    #26 - AdminService 작성 및 테스트 수정
    Id, Nickname, Email의 중복 검사 기능 추가
    `SpringBootTest`에서 `MockitoExtension`을 활용한 테스트로 변경
    테스트 완료

--- src/main/java/org/dry/service/AdminServiceImpl.java
+++ src/main/java/org/dry/service/AdminServiceImpl.java
@@ -1,5 +1,6 @@
 package org.dry.service;
 
+import lombok.NoArgsConstructor;
 import org.dry.entity.Admin;
 import org.dry.mapper.AdminMapper;
 import org.dry.repository.AdminRepository;
@@ -7,6 +8,7 @@ import org.dry.vo.IdAndPassword;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+@NoArgsConstructor
 @Service
 public class AdminServiceImpl implements AdminService {
     private AdminMapper adminMapper;
@@ -17,9 +19,44 @@ public class AdminServiceImpl implements AdminService {
         this.adminMapper = adminMapper;
         this.adminRepository = adminRepository;
     }
+    
+    // mock 테스트용 생성자
+    public AdminServiceImpl(AdminMapper adminMapper, AdminRepository adminRepository, String mock) {
+        this.adminMapper = adminMapper;
+        this.adminRepository = adminRepository;
+    }
+
 
     @Override
     public Admin login(IdAndPassword idAndPassword) {
         return adminMapper.selectByIdAndPassword(idAndPassword);
+    }
+
+    @Override
+    public Admin signUp(Admin admin) {
+        return adminRepository.save(admin);
+    }
+
+    // 항목 중복 검사
+    @Override
+    public boolean isIdTaken(String id) {
+        Admin admin = adminMapper.selectById(id);
+        boolean isIdTaken = (admin != null);
+        return isIdTaken;
+    }
+    
+    @Override
+    public boolean isNicknameTaken(String nickname) {
+        Admin admin = adminMapper.selectByNickname(nickname);
+        boolean isNicknameTaken = (admin != null);
+        return isNicknameTaken;
+    }
+
+
+    @Override
+    public boolean isEmailTaken(String email) {
+        Admin admin = adminMapper.selectByEmail(email);
+        boolean isEmailTaken = (admin != null);
+        return isEmailTaken;
     }
 }
