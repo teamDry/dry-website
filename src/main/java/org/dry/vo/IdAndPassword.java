5d1f114425a8f26660fb0719dd99fa910b6c14a2
un <mapzine123@naver.com>
2023-08-19 01:29:11 +0900

    #30 - IdAndPassword VO객체 역할에 맡게 변경
    불필요한 메서드 삭제

--- src/main/java/org/dry/vo/IdAndPassword.java
+++ src/main/java/org/dry/vo/IdAndPassword.java
@@ -12,16 +12,4 @@ import java.util.Objects;
 public class IdAndPassword {
     private String id;
     private String password;
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (!(o instanceof IdAndPassword that)) return false;
-        return Objects.equals(id, that.id) && Objects.equals(password, that.password);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(id, password);
-    }
 }
