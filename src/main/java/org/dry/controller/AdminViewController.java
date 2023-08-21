42f77d8fe882bfc71fa8711dcc157a1c5480b122
un <mapzine123@naver.com>
2023-08-22 03:09:09 +0900

    #30 - AdminViewController 기능 추가 및 수정
    추가
    - code를 가지고 로그인 화면으로 오면 code 처분
    - 코드를 가지고 회원가입을 하러 왔을시 코드 유효성 검증
    
    수정
    - Restful하게 url 수정, 그에 따른 매핑값 수정

--- src/main/java/org/dry/controller/AdminViewController.java
+++ src/main/java/org/dry/controller/AdminViewController.java
@@ -1,15 +1,31 @@
 package org.dry.controller;
 
+import org.dry.service.InviteCodesService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 
-@RequestMapping("/admin")
+@RequestMapping("/admins")
 @Controller
 public class AdminViewController {
+    private final InviteCodesService inviteCodesService;
+
+    @Autowired
+    public AdminViewController(InviteCodesService inviteCodesService) {
+        this.inviteCodesService = inviteCodesService;
+    }
+
     @GetMapping("/login") // 로그인 view 화면
     public String login() {
+        return "admin/login";
+    }
+
+    @GetMapping("/login/code") // 로그인 view 화면
+    public String login(@RequestParam String code) { // 코드 정보를 가지고 회원가입을 했으면?
+        inviteCodesService.removeCode(code); // 해당 코드 삭제 ( 이용불가 )
         return "admin/login";
     }
 
@@ -23,9 +39,19 @@ public class AdminViewController {
         return "error/admin-login-failed";
     }
 
-    @GetMapping("/sign-up") // 회원 가입 view 화면
+    @GetMapping("/sign-up") // 회원 가입 view 화면 (삭제 예정)
     public String getSignUp() {
         return "admin/sign-up";
+    }
+
+    // 코드를 가지고 회원가입 하러 옴
+    @GetMapping("/member/sign-up")
+    public String memberSignUp(@RequestParam String code) {
+        if(inviteCodesService.isExist(code)) { // 만약 유효한 코드라면
+            return "redirect:/admins/sign-up?code=" + code; //
+        } else {
+            return "redirect:/admins/loginFail"; // 아니면 에러 페이지
+        }
     }
 
 }
