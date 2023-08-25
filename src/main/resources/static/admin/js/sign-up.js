86950841bb3402dcc4e086f5e3648df9adc63e15
un <mapzine123@naver.com>
2023-08-26 00:01:55 +0900

    #37 - signup View 작성
    디자인 및 기능 개선
    - id 유효성 패턴 추가
    - nickname 유효성 패턴 추가
    - 패턴 검증 시 즉시 피드백 ( 기존엔 느렸음 )

--- src/main/resources/static/admin/js/sign-up.js
+++ src/main/resources/static/admin/js/sign-up.js
@@ -1,21 +1,12 @@
 let isIdOk = false;
 let isPasswordOk = false;
+let isPasswordCheckOk = false;
 let isNicknameOk = false;
 let isEmailOk = false;
 
-// Test용 DATA 세팅
-function dataSetting() {
-    document.getElementById("adminId").value = "abc";
-    document.getElementById("adminPassword").value = "abcde";
-    document.getElementById("adminPassword-check").value = "abcde";
-    document.getElementById("adminNickname").value = "abcNick";
-    document.getElementById("adminEmail").value = "abc@naver.com";
-}
-
-
 // 회원가입 버튼 눌렸을때 ajax 처리
 function performSignUp() {
-    if(!isIdOk && !isPasswordOk && !isNicknameOk && !isEmailOk) {
+    if(!isIdOk && !isPasswordOk && !isPasswordCheckOk && !isNicknameOk && !isEmailOk) {
         return;
     }
 
@@ -25,10 +16,10 @@ function performSignUp() {
     const code = urlParams.get('code'); // 'John'
 
     const admin = {
-        id: document.getElementById("adminId").value,
-        password: document.getElementById("adminPassword").value,
-        nickname: document.getElementById("adminNickname").value,
-        email: document.getElementById("adminEmail").value
+        id: document.getElementById("id").value,
+        password: document.getElementById("password").value,
+        nickname: document.getElementById("nickname").value,
+        email: document.getElementById("email").value
     };
 
     fetch("/api/admins/sign-up", {
@@ -61,12 +52,16 @@ function clickCancelButton() {
 }
 
 // ID 중복 검사
-const idElement = document.getElementById("adminId");
+const idElement = document.getElementById("id");
 idElement.addEventListener("focusout", idCheckEvent);
 
 function idCheckEvent() {
-    // 만약 값이 빈칸이면 바로 return ( fetch 실행 안함 )
+    // 만약 값이 빈칸이면 바로 return ( 실행 안함 )
     if(dontRunWhenNothingValueInComponent(idElement)) {
+        return;
+    } else if(isValidId(idElement.value)) {
+        isIdOk = false;
+        changeBackgroundColor(document.getElementById("id-check"), isIdOk); // 되는 값인지 안되는 값인지 표시
         return;
     }
 
@@ -76,41 +71,51 @@ function idCheckEvent() {
     })
         .then(response => { // 응답이 곧 유효성
             // 서버에 물어봤을때 ok(되는아이디)면 true, 아니면 false
-            isIdOk = response.ok;
+            isIdOk = response.ok
+            changeBackgroundColor(document.getElementById("id-check"), isIdOk); // 되는 값인지 안되는 값인지 표시
         })
-    changeBackgroundColor(idElement, isIdOk); // 되는 값인지 안되는 값인지 표시
 }
 
-// PASSWORD CHECK 확인
-const password = document.getElementById("adminPassword");
-const passwordCheck = document.getElementById("adminPassword-check");
+// PASSWORD 확인
+const password = document.getElementById("password");
+const passwordCheck = document.getElementById("passwordCheck");
 
-password.addEventListener("focusout", passwordCheckEvent);
+password.addEventListener("focusout", passwordValidCheckEvent);
 passwordCheck.addEventListener("focusout", passwordCheckEvent);
 
-function passwordCheckEvent() {
-    // 만약 값이 빈칸이면 바로 return ( fetch 실행 안함 )
+function passwordValidCheckEvent() {
     if(dontRunWhenNothingValueInComponent(password)) {
         return;
-    } else if(password.value.trim().length <= 7) {
-        isPasswordOk = false; // 비밀번호 길이가 7글자 이하면 안됨
-    } else if(password.value.trim() == passwordCheck.value.trim()) {
+    } else if(isValidPassword(password.value.trim())) {
         isPasswordOk = true;
     } else {
         isPasswordOk = false;
     }
-    changeBackgroundColor(passwordCheck, isPasswordOk); // 되는 값인지 안되는 값인지 표시
+    changeBackgroundColor(document.getElementById("password-check"), isPasswordOk); // 되는 값인지 안되는 값인지 표시
+}
+
+function passwordCheckEvent() {
+    // 만약 값이 빈칸이면 바로 return ( 실행 안함 )
+    if(dontRunWhenNothingValueInComponent(passwordCheck)) {
+        return;
+    }
+    isPasswordCheckOk = password.value.trim() == passwordCheck.value.trim() && isPasswordOk;
+    changeBackgroundColor(document.getElementById("passwordCheck-check"), isPasswordCheckOk); // 되는 값인지 안되는 값인지 표시
 }
 
 
 // NICKNAME 중복 검사
-const nicknameElement = document.getElementById("adminNickname");
+const nicknameElement = document.getElementById("nickname");
 nicknameElement.addEventListener("focusout", nicknameCheckEvent);
 
 function nicknameCheckEvent() {
     // 만약 값이 빈칸이면 바로 return ( fetch 실행 안함 )
     if(dontRunWhenNothingValueInComponent(nicknameElement)) {
         return; // fetch 실행 X
+    } else if(isValidNickname(nicknameElement.value)) {
+        isNicknameOk = false;
+        changeBackgroundColor(document.getElementById("nickname-check"), isIdOk); // 되는 값인지 안되는 값인지 표시
+        return;
     }
 
     fetch(`/api/admins/nickname/check?nickname=${nicknameElement.value}`, {
@@ -120,12 +125,12 @@ function nicknameCheckEvent() {
         .then(response => { // 응답이 곧 유효성
             // 서버에 물어봤을때 ok(되는 닉네임)면 true, 아니면 false
             isNicknameOk = response.ok;
+            changeBackgroundColor(document.getElementById("nickname-check"), isNicknameOk); // 되는 값인지 안되는 값인지 표시
         })
-    changeBackgroundColor(nicknameElement, isNicknameOk); // 되는 값인지 안되는 값인지 표시
 }
 
 // EMAIL 유효성 검사
-const emailElement = document.getElementById("adminEmail");
+const emailElement = document.getElementById("email");
 emailElement.addEventListener("focusout", emailCheckEvent);
 
 function emailCheckEvent() {
@@ -134,7 +139,7 @@ function emailCheckEvent() {
     } if(!isValidEmail(emailElement.value)) { // 이메일 형식인지 검사
         // 이메일 형식이 아니라면?
         isEmailOk = false; // 안되는 이메일입니다.
-        changeBackgroundColor(emailElement, isEmailOk); // 되는 값인지 안되는 값인지 표시
+        changeBackgroundColor(document.getElementById("email-check"), isEmailOk); // 되는 값인지 안되는 값인지 표시
         return; // fetch 실행 X
     }
 
@@ -144,9 +149,15 @@ function emailCheckEvent() {
     })
         .then(response => { // 응답이 곧 유효성
             // 서버에 물어봤을때 ok(되는 닉네임)면 true, 아니면 false
-            isEmailOk = response.ok;
+            isEmailOk = response.ok
+            changeBackgroundColor(document.getElementById("email-check"), isEmailOk); // 되는 값인지 안되는 값인지 표시
         })
-    changeBackgroundColor(emailElement, isEmailOk); // 되는 값인지 안되는 값인지 표시
+}
+
+// 비밀번호가 8글자 이상에 특수문자 반드시 포함할 것
+function isValidPassword(password) {
+    const passwordRegex = /^(?=.*[!@#$%^&*])[a-zA-Z0-9!@#$%^&*]{8,}$/;
+    return passwordRegex.test(password);
 }
 
 // 이메일 주소의 유효성을 검사하는 정규 표현식 검사 함수
@@ -155,16 +166,24 @@ function isValidEmail(email) {
     return emailPattern.test(email);
 }
 
+function isValidId(id) {
+    const regex = /^[가-힣]{0}$|^(?![!@#$%^&* ]+$)[a-zA-Z0-9 ]{4,}$/;
+    return regex.text(id);
+}
+
+function isValidNickname(nickname) {
+    const regex = /^(?![!@#$%^&* ]+$)[a-zA-Z0-9]+$/;
+    return regex.text(nickname);
+}
+
 
 // 가능 or 불가능한 항목에 대해 배경색으로 표시
 // TODO : 추후 수정 예정
 function changeBackgroundColor(component, isOk) {
     if(isOk) {
-        component.style.backgroundColor = "black";
-        component.style.color = "white";
+        component.style.color = "#279EFF";
     } else {
-        component.style.backgroundColor = "red";
-        component.style.color = "black";
+        component.style.color = "#F94C10";
     }
 }
 
@@ -174,4 +193,3 @@ function dontRunWhenNothingValueInComponent(component) {
         return true;
     }
 }
-
